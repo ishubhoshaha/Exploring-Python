b9b452cb3aee0b5f7975021424a6954ef979436e
Shubho Shaha <ishubhoshaha@users.noreply.github.com>
2017-04-16 18:06:16 +0600

    Update deque.py

--- deque.py
+++ deque.py
@@ -4,27 +4,27 @@ dq = deque("123")
 print (dq)
 
 print("--------------------")
-dq.append(4)
+dq.append(4) # append to end
 print (dq)
 
 print("--------------------")
-dq.appendleft('0')
+dq.appendleft('0') # append on 0th postion
 print (dq)
 
 print("--------------------")
-dq.pop()
+dq.pop() # Pop from end
 print (dq)
 
 print("--------------------")
-dq.popleft()
+dq.popleft() # pop from 0th postion
 print (dq)
 
 print("--------------------")
-dq.extend([4,5])
+dq.extend([4,5]) # append a list to end of dq
 print (dq)
 
 print("--------------------")
-dq.extendleft([0,-1])
+dq.extendleft([0,-1]) # append a list at begining
 print (dq)
 
 print("--------------------")
