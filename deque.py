cd20e8d1ba18e2b8b0c854b72fdf4960c551cc86
Shubho Shaha <ishubhoshaha@users.noreply.github.com>
2017-04-16 17:58:03 +0600

    Create deque.py

---
+++ deque.py
@@ -0,0 +1,27 @@
+"""using Python3"""
+from collections import deque
+dq = deque("123")
+print (dq)
+
+print("--------------------")
+dq.append(4)
+print (dq)
+
+print("--------------------")
+dq.appendleft('0')
+print (dq)
+
+print("--------------------")
+dq.pop()
+print (dq)
+
+print("--------------------")
+dq.popleft()
+print (dq)
+
+print("--------------------")
+dq.extend([4,5])
+print (dq)
+print("--------------------")
+dq.extendleft([0,-1])
+print (dq)
