a442dcd57a6e73abdab755f46efd9ea5901d65df
Shubho Shaha <ishubhoshaha@users.noreply.github.com>
2017-04-16 18:02:13 +0600

    Update deque.py

--- deque.py
+++ deque.py
@@ -22,6 +22,15 @@ print (dq)
 print("--------------------")
 dq.extend([4,5])
 print (dq)
+
 print("--------------------")
 dq.extendleft([0,-1])
+print (dq)
+
+print("--------------------")
+dq.rotate(1) # rotate left
+print (dq)
+
+print("--------------------")
+dq.rotate(-1) # rotate right
 print (dq)
